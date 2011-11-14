7c3309f5bc7ed0db5528cadf8a53be5e9c4a80f4
Paul Irofti <paul@irofti.net>
2011-11-14 03:55:34 +0200

    Add interrupt vector in trap.c.

---
+++ arch/ia64/ia64/trap.c
@@ -0,0 +1,151 @@
+/* $OpenBSD$ */
+/*
+ * Copyright (c) 2011 Paul Irofti <pirofti@openbsd.org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * amd64 Trap and System call handling
+ */
+#undef	TRAP_SIGDEBUG
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/proc.h>
+#include <sys/user.h>
+#include <sys/acct.h>
+#include <sys/kernel.h>
+#include <sys/signal.h>
+#include <sys/syscall.h>
+#include <sys/reboot.h>
+#include <sys/pool.h>
+
+#include <uvm/uvm_extern.h>
+
+#include <machine/cpu.h>
+#include <machine/reg.h>
+#include <machine/trap.h>
+#ifdef DDB
+#include <machine/db_machdep.h>
+#endif
+
+#include "isa.h"
+
+#ifdef KGDB
+#include <sys/kgdb.h>
+#endif
+
+void trap(struct trapframe *);
+
+const char *trap_type[] = {
+	"VHPT Translation",			/* 0 */
+	"Instruction TLB",			/* 1 */
+	"Data TLB",				/* 2 */
+	"Alternate Instruction TLB",		/* 3 */
+	"Alternate Data TLB",			/* 4 */
+	"Data Nested TLB",			/* 5 */
+	"Instruction Key Miss",			/* 6 */
+	"Data Key Miss",			/* 7 */
+	"Dirty-Bit",				/* 8 */
+	"Instruction Access-Bit",		/* 9 */
+	"Data Access-Bit",			/* 10 */
+	"Break Instruction",			/* 11 */
+	"External Interrupt",			/* 12 */
+	"Reserved 13",				/* 13 */
+	"Reserved 14",				/* 14 */
+	"Reserved 15",				/* 15 */
+	"Reserved 16",				/* 16 */
+	"Reserved 17",				/* 17 */
+	"Reserved 18",				/* 18 */
+	"Reserved 19",				/* 19 */
+	"Page Not Present",			/* 20 */
+	"Key Permission",			/* 21 */
+	"Instruction Access Rights",		/* 22 */
+	"Data Access Rights",			/* 23 */
+	"General Exception",			/* 24 */
+	"Disabled FP-Register",			/* 25 */
+	"NaT Consumption",			/* 26 */
+	"Speculation",				/* 27 */
+	"Reserved 28",				/* 28 */
+	"Debug",				/* 29 */
+	"Unaligned Reference",			/* 30 */
+	"Unsupported Data Reference",		/* 31 */
+	"Floating-point Fault",			/* 32 */
+	"Floating-point Trap",			/* 33 */
+	"Lower-Privilege Transfer Trap",	/* 34 */
+	"Taken Branch Trap",			/* 35 */
+	"Single Step Trap",			/* 36 */
+	"Reserved 37",				/* 37 */
+	"Reserved 38",				/* 38 */
+	"Reserved 39",				/* 39 */
+	"Reserved 40",				/* 40 */
+	"Reserved 41",				/* 41 */
+	"Reserved 42",				/* 42 */
+	"Reserved 43",				/* 43 */
+	"Reserved 44",				/* 44 */
+	"IA-32 Exception",			/* 45 */
+	"IA-32 Intercept",			/* 46 */
+	"IA-32 Interrupt",			/* 47 */
+	"Reserved 48",				/* 48 */
+	"Reserved 49",				/* 49 */
+	"Reserved 50",				/* 50 */
+	"Reserved 51",				/* 51 */
+	"Reserved 52",				/* 52 */
+	"Reserved 53",				/* 53 */
+	"Reserved 54",				/* 54 */
+	"Reserved 55",				/* 55 */
+	"Reserved 56",				/* 56 */
+	"Reserved 57",				/* 57 */
+	"Reserved 58",				/* 58 */
+	"Reserved 59",				/* 59 */
+	"Reserved 60",				/* 60 */
+	"Reserved 61",				/* 61 */
+	"Reserved 62",				/* 62 */
+	"Reserved 63",				/* 63 */
+	"Reserved 64",				/* 64 */
+	"Reserved 65",				/* 65 */
+	"Reserved 66",				/* 66 */
+	"Reserved 67",				/* 67 */
+};
+int	trap_types = sizeof trap_type / sizeof trap_type[0];
+
+#ifdef DEBUG
+int	trapdebug = 0;
+#endif
+
+#define	IDTVEC(name)	__CONCAT(X, name)
+
+#ifdef TRAP_SIGDEBUG
+static void frame_dump(struct trapframe *);
+#endif
+
+/*
+ * trap(frame):
+ *	Exception, fault, and trap interface to BSD kernel. This
+ * common code is called from assembly language IVT gate entry
+ * routines that prepare a suitable stack frame, and restore this
+ * frame after the exception has been processed.
+ */
+/*ARGSUSED*/
+void
+trap(struct trapframe *frame)
+{
+}
+
+#ifdef TRAP_SIGDEBUG
+static void
+frame_dump(struct trapframe *tf)
+{
+}
+#endif
